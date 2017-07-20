b2206c86686e807457bfc22e5b2df87efb24bf19
Andrew Mogg <andrewmogg@Andrews-MacBook-Pro.local>
2017-07-20 14:46:44 -0400

    created sign up form

---
+++ components/SignUpForm.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { FormLabel, FormInput, Button } from 'react-native-elements';
+import axios from 'axios';
+
+const ROOT_URL = "https://us-central1-one-time-password-e1b43.cloudfunctions.net";
+
+class SignUpForm extends Component {
+  state = { phone: '' };
+
+  // by setting method to a fat arrow function we dont how to bind it
+  // in the onPress call
+  handleSubmit = async () => {
+    try {
+      //wait for promise to be returned before going to the next line
+      await axios.post(`${ROOT_URL}/createUser`, { phone: this.state.phone });
+      await axios.post(`${ROOT_URL}/requestOneTimePassword`, { phone: this.state.phone });
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+
+  render() {
+    return(
+      <View>
+      <View style={{ marginBottom: 10 }}>
+        <FormLabel>Enter Phone Number</FormLabel>
+        <FormInput
+          value={this.state.phone}
+          onChangeText={phone => this.setState({ phone })}
+        />
+        </View>
+        <Button
+          onPress={this.handleSubmit}
+          title="Submit" />
+      </View>
+    )
+  }
+}
+
+export default SignUpForm;
